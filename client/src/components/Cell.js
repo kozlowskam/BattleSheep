8f5bc7574f5de677c32cd31a3c709ac95164f33d
Lennart <lwijers@gmail.com>
2018-05-31 17:43:17 +0200

    final commit thursday

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -12,15 +12,27 @@ class Cell extends PureComponent {
     super(props)
     }
 
+cellColors = {
+  'unexplored' : (10, 245, 22),
+  'exploredEmpty' : (255, 8, 82),
+  'exploredOccupied' : (255,255,255),
+}
 
+  
+  updateCell =(cell) => {
+    if (testBoard[cell.props.position[0]]
+        [cell.props.position[1]]
+        .occupied) {
+     
+  }}
 
-    
   onCellClick = () => {
-    console.log(this.props.cellPosition, 'Log made in line 13 cell.js')
-    makeShot(testBoard, this.props.cellPosition)
+    console.log(this.props.position, 'Log made in line 13 cell.js')
+    makeShot(testBoard, this.props.position)
     request
       .put("http://localhost:4000/games/:id")
       .send({}) 
+      this.updateCell(this)
     return this.props.cellPosition
   }  
 
